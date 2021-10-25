b9e713ed5297494cf98436256b072754314c6391
sam <s.gelernter@gmail.com>
2021-10-25 11:26:44 -0400

    Complete through children method

--- tic-tac-toe-ai/skeleton/lib/tic_tac_toe_node.rb
+++ tic-tac-toe-ai/skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,14 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+  
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos 
+    @current_mark = @next_mover_mark == :x ? :o : :x 
   end
 
   def losing_node?(evaluator)
@@ -13,5 +20,18 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    @children = [] 
+    (0..2).each do |row|
+      (0..2).each do |col|
+        pos = row, col
+        if @board.empty?(pos)
+          dup = @board.dup
+          dup[pos] = @current_mark 
+          @children << TicTacToeNode.new(dup, @current_mark, pos)
+        end
+      end
+    end
+    @children   
   end
+
 end
